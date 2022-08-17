665bf42a5e0f9b2a4e42c6e5562b0db279d87496
gokceeksioglu <gokceeksioglu@hotmail.com>
2022-08-17 18:38:36 +0100

    day1_basic Navigations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/SeleniumTest.java
@@ -9,11 +9,12 @@ public class SeleniumTest {
     public static void main(String[] args) {
 
         //1.Setting up the web driver manager
+        //We create our "Browser Driver"
         WebDriverManager.chromedriver().setup();
 
         //2.Create instance of the chrome driver
 
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver=new ChromeDriver(); //this line open web browser
 
         //3.Test if driver is working as expected
 
