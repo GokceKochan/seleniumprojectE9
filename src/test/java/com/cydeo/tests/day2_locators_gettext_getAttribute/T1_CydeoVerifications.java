3815a073cb686e9a0ea14741a2ef601abce40849
gokceeksioglu <gokceeksioglu@hotmail.com>
2022-08-18 16:33:52 +0100

    day2 Cydeo Verification

---
+++ src/test/java/com/cydeo/tests/day2_locators_gettext_getAttribute/T1_CydeoVerifications.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day2_locators_gettext_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_CydeoVerifications {
+
+    public static void main(String[] args) {
+        // TC #1: Cydeo practice tool verifications
+        // 1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 2. Go to https://practice.cydeo.com
+        driver.navigate().to("https://practice.cydeo.com");
+        // 3. Verify URL contains
+        // Expected: cydeo
+
+        String expectedURl = "cydeo";
+        String actualURL = driver.getCurrentUrl();
+
+        if(actualURL.contains(expectedURl)){
+            System.out.println("URL verification PASSED");
+        }else{
+            System.out.println("URL verification failed");
+        }
+        // 4. Verify title:
+        // Expected: Practice
+
+        String expectedTitle = "Practice";
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASSED");
+        }else {
+            System.out.println("Title verification FAILED");
+        }
+
+        driver.close();
+
+    }
+}
